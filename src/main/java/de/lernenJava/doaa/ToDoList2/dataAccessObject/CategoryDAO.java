ff77eb3ac1d2d4ec3928360260ae557fc68cf6b5
Ahmed Marzouk <egnuke+github@gmail.com>
2020-12-05 22:43:14 +0100

    DAO classes review comments

--- src/main/java/de/lernenJava/doaa/ToDoList2/dataAccessObject/CategoryDAO.java
+++ src/main/java/de/lernenJava/doaa/ToDoList2/dataAccessObject/CategoryDAO.java
@@ -1,4 +1,7 @@
 package de.lernenJava.doaa.ToDoList2.dataAccessObject;
+//[REVIEW] please remove "Object" from package name
+// Also, package names should be all in lower case as per Java naming conventions.
+// https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html
 
 import de.lernenJava.doaa.ToDoList2.models.Category;
 import org.springframework.data.repository.CrudRepository;
@@ -7,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CategoryDAO extends CrudRepository<Category,Integer> {
+public interface CategoryDAO extends CrudRepository<Category, Integer> {    //[REVIEW] spaces :)
     @Override
     List<Category> findAll();
 }
--- src/main/java/de/lernenJava/doaa/ToDoList2/dataAccessObject/TaskDAO.java
+++ src/main/java/de/lernenJava/doaa/ToDoList2/dataAccessObject/TaskDAO.java
@@ -1,14 +1,15 @@
 package de.lernenJava.doaa.ToDoList2.dataAccessObject;
 
-import de.lernenJava.doaa.ToDoList2.models.Tasks;
-import de.lernenJava.doaa.ToDoList2.toDoServices.TasksServices;
+import de.lernenJava.doaa.ToDoList2.models.Tasks;   //[REVIEW] please remember to remove unused imports.
+// You can Intellij's [Code -> Optimize Imports] to do it automatically.
+// You can also configure Intellij to optimize imports on save.
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TaskDAO extends CrudRepository<Tasks,Integer> {
+public interface TaskDAO extends CrudRepository<Tasks, Integer> {    //[REVIEW] missing space :)
     @Override
     List<Tasks> findAll();
 }
